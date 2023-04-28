e0f3d779b5d08eda98217807089610705aa206e5
drama <loginovzaxar04@gmail.com>
2023-04-28 21:49:59 +0300

    123

--- AutoParts/Objects/Users.cs
+++ AutoParts/Objects/Users.cs
@@ -8,5 +8,11 @@ namespace AutoParts.Objects
 {
     internal class Users
     {
+        public int ID_Employee { get; set; }
+        public string Phone { get;  set; }
+         public string Email { get;  set; }
+        public string FName { get;  set; }
+        public string LName { get;  set; }
+        public string PassportData { get;  set; }
     }
 }
